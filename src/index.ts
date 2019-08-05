191360071fc1a96b89c6c40c269482ad5d127e8f
isthisnagee <isthisnagee@gmail.com>
2019-08-04 23:02:53 -0700

    Describe file structure in index.ts file
    I feel like having comments in the file describing the structure of
    things is incredibly useful, but also hard to maintain.
    
    Keeping it local to the file means that it is hard for developers to
    ignore. But it also means that when changing how things work, developers
    have to update it. If this update step is ignored, having all that text
    there will just create confusion for new developers.
    
    Keeping the comments up to date is something that needs to be watched
    out for during code reviews.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,47 @@
-alert('hello world');
+(function() {
+  // ************
+  // General Info
+  // ************
+  //
+  // Everything in this file lives in an Immediately Invoked Function
+  // Expression. We do this to avoid polluting the global namespace.
+  //
+  // ==============
+  // File Structure
+  // ==============
+  //
+  // Each section contains a title and a paragraph defining its use.
+  // All functions are defines at the bottom of the file, taking advantage
+  // of function hoisting in JavaScript. Functions should go in their appropriate
+  // section. If a section gets longer than 100 lines, feel free to move it to its
+  // own file.
+  //
+  // We will follow a Space Shuttle style of programming. Every branch and
+  // condition bust be considered and accounted for. Why? I just want an exercise
+  // in doing it :)
+  //
+  // Each function should have its input and output types defined.
+
+  // *******
+  // GLOBALS
+  // *******
+  //
+  // This section defines all HTML elements, magic numbers, and string constants.
+
+  const TEXT = {
+    greeting: 'hello world',
+  };
+
+
+  // ****
+  // MAIN
+  // ****
+  //
+  // This section is _only_ for the main function.
+
+  main();
+
+  function main(): void {
+    alert(TEXT.greeting);
+  }
+})();
